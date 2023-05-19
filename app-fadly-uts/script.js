2500ed71f7799b82abe7a7c6e8382874f851031c
Bagus Fadly Hidayatullah <shizne.jr@gmail.com>
2023-05-19 11:07:45 +0700

    v1.0

---
+++ app-fadly-uts/script.js
@@ -0,0 +1,20 @@
+const nilai = "900";
+if (nilai=>85 nilai<=100) {
+    document.write("A");
+} else if (nilai>=79) {
+    document.write("A-");
+} else if(nilai>=74) {
+    document.write("B+");
+} else if (nilai>=69) {
+    document.write("B");
+} else if (nilai>=64) {
+    document.write("B-");
+} else if (nilai>=59) {
+    document.write("C+");
+} else if (nilai>=54) {
+    document.write("C");
+} else if (nilai>=41) {
+    document.write("D");
+} else {
+    document.write("E");
+}
