f6ae5ad626fe31296ffaa32436e84767402cc6df
Bagus Fadly Hidayatullah <shizne.jr@gmail.com>
2023-05-31 15:01:55 +0700

    modul5

---
+++ modul5/ifstatement/if.js
@@ -0,0 +1,8 @@
+var nama = prompt("Masukkan nama Anda:");
+
+if (nama) {
+  document.write("Halo, " + nama + "!");
+  
+} else {
+  document.write("Silakan masukkan nama Anda!");
+}
---
+++ modul5/ternaryoperator/ternary.js
@@ -0,0 +1,5 @@
+var angka = prompt("Masukkan Angka");
+
+var jenisAngka = angka >= 0 ? "Positif" : "Negatif";
+
+document.write("Angka adalah angka " + jenisAngka);
